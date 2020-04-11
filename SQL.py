bbce07605d5adcd9ac498993be358fe5d02e7490
shin6949 <devshin6949@gmail.com>
2020-04-11 23:40:11 +0900

    IP 변경 대응

--- SQL.py
+++ SQL.py
@@ -17,6 +17,6 @@ def make_chatonnection():
 
 def make_backupconnection():
     # Google Cloud
-    conn = pymysql.connect(host='35.247.10.252', user='OXBot', password='{DB_PASSWORD}', db='MS2OX', charset='utf8mb4')
+    conn = pymysql.connect(host='{DB_HOST}', user='OXBot', password='{DB_PASSWORD}', db='MS2OX', charset='utf8mb4')
 
     return conn
